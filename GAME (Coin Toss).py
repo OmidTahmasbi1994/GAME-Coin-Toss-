c38a9e98190dc257f300c3e1532011078794f1e8
Omid Tahmasbi <91671463+OmidTahmasbi1994@users.noreply.github.com>
2021-12-19 18:50:38 +0330

    Add files via upload

---
+++ GAME (Coin Toss).py
@@ -0,0 +1,35 @@
+'''
+    GAME:
+        Coin Toss
+        
+'''
+
+import random
+
+print('Heads = 0    &   Tails = 1')
+
+n = random.randrange(0,1)
+f='no'
+
+a=int(input('Your Choice is: '))
+
+    
+if a != n:
+    if a == 0:
+        print('You Lose , It Is Tails')
+        
+    else:
+            print('You Lose , It Is Heads')
+        
+else:
+    print('You Win , Thank You')
+            
+            
+
+        
+        
+    
+    
+
+
+        
